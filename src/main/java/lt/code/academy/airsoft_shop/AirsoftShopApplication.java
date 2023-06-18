1691be298c71c878f9d3774bcd561634f1e6c6f6
Nilsas9 <nilsas@inbox.lt>
2023-06-18 11:27:49 +0300

    konfiguruoju

---
+++ src/main/java/lt/code/academy/airsoft_shop/AirsoftShopApplication.java
@@ -0,0 +1,13 @@
+package lt.code.academy.airsoft_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsoftShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirsoftShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/code/academy/airsoft_shop/AirsoftShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.code.academy.airsoft_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsoftShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
