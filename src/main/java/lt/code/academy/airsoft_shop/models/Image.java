3f3cd006de2bd6a44fec38d187cd794dca8c3e43
Nilsas9 <nilsas@inbox.lt>
2023-06-18 18:49:17 +0300

    Images

---
+++ src/main/java/lt/code/academy/airsoft_shop/models/Image.java
@@ -0,0 +1,39 @@
+package lt.code.academy.airsoft_shop.models;
+
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private  Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "originalFilename")
+    private String originalFilename;
+    @Column(name = "size")
+    private long size;
+    @Column(name = "contentType")
+    private String contentType;
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+
+    @Lob
+//    saugos LOB bloke
+    private byte[] bytes;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+//    jei bus daug prekiu, tureciau pasikeisti i Lazy
+    private Product product;
+}
--- src/main/java/lt/code/academy/airsoft_shop/services/ProductService.java
+++ src/main/java/lt/code/academy/airsoft_shop/services/ProductService.java
@@ -2,10 +2,13 @@ package lt.code.academy.airsoft_shop.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lt.code.academy.airsoft_shop.models.Image;
 import lt.code.academy.airsoft_shop.models.Product;
 import lt.code.academy.airsoft_shop.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,9 +25,40 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product) {
-        log.info("saving new {}", product);
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageTOProduct(image1);
+        }
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImageTOProduct(image2);
+        }
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImageTOProduct(image3);
+        }
+
+        log.info("saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor());
+        Product productFromDb = productRepository.save(product);
+        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    private Image toImageEntity(MultipartFile file) throws IOException {
+
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFilename(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
+
     }
 
     public void deleteProduct(Long id) {
